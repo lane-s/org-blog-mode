543af45a36ec3545ae4f9e8774cb01512b08f799
Lane Spangler <lspangler@protonmail.com>
2019-06-09 15:46:22 -0500

    Use local hash table to store path to posts

--- org-blog.mode.el
+++ org-blog.mode.el
@@ -7,15 +7,46 @@
 
 ;;; Org Blog Mode
 ;;;; Config
- (defvar org-blog-mode-server-url "http://localhost:3000/api")
+(defvar org-blog-mode-server-url "http://localhost:3000/api"
+  "The full url of the org-blog server to use")
+(defvar org-blog-mode-local-data-file (substitute-in-file-name "$HOME/.org-blog-mode")
+  "The full file path specifying where to store local data. Default is ~/.org-blog-mode")
 
 ;;;; Helper
+(defvar org-blog-mode--cached-file-hash nil)
+
 (defvar org-blog-mode--handle-error
   (cl-function (lambda (&rest args &key error-thrown &allow-other-keys)
                  (message "An error occurred: %S" error-thrown))))
 
 (defun org-blog-mode--get-current-filename ()
+  "Get the filename (not the full path) of the current file"
   (->> (current-buffer) buffer-file-name (s-split "/") -last-item))
+
+(defun org-blog-mode--get-or-create-file-hash ()
+  "Get the hash table that maps from filenames to
+  fully qualified file paths. If the table is not
+  cached, then cache it"
+  (unless org-blog-mode--cached-file-hash
+    (setq org-blog-mode--cached-file-hash
+          (with-current-buffer (find-file org-blog-mode-local-data-file)
+            (-let [hash-from-file (ignore-errors
+                                    (read (buffer-substring (point-min) (point-max))))]
+              (kill-buffer (current-buffer))
+              (or hash-from-file (make-hash-table :test 'equal))))))
+  org-blog-mode--cached-file-hash)
+
+(defun org-blog-mode--update-file-hash (filename fullpath)
+  "Update the hash table so that `filename' maps to `fullpath'"
+  (-let [file-map (org-blog-mode--get-or-create-file-hash)]
+    (puthash filename fullpath file-map)
+    (setq org-blog-mode--cached-file-hash file-map)
+    (with-current-buffer (find-file org-blog-mode-local-data-file)
+      (erase-buffer)
+      (goto-char (point-min))
+      (insert (prin1-to-string file-map))
+      (save-buffer)
+      (kill-buffer (current-buffer)))))
 
 (defun org-blog-mode--post (preview)
   (-let* ((post (buffer-substring (point-min)
@@ -24,14 +55,22 @@
           (body (json-encode `(("filename" . ,filename)
                                ("post" . ,post)
                                ("preview" . ,preview)))))
+
     (request (s-concat org-blog-mode-server-url "/post")
              :type "POST"
              :data body
              :headers '(("Content-Type" . "application/json"))
              :parser 'json-read
-             :success (cl-function (lambda (&key data &allow-other-keys)
-                                     (message "Post successful")))
-             :error org-blog-mode--handle-error)))
+             :success (if (string= preview "false")
+                        (cl-function (lambda (&key data &allow-other-keys)
+                                       (message "Post successful")))
+                        (cl-function (lambda (&key data &allow-other-keys)
+                                       (message "Post sent for preview"))))
+             :error org-blog-mode--handle-error)
+
+    (when (string= preview "false")
+      (org-blog-mode--update-file-hash
+       filename (buffer-file-name (current-buffer))))))
 
 (defun org-blog-mode--delete (filename)
   (request (s-concat org-blog-mode-server-url "/post/" filename)
