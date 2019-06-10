12987d3e81d45b6ab317939f07252a8c85adf5da
Lane Spangler <lspangler@protonmail.com>
2019-06-09 21:26:47 -0500

    Add command to open post at point, bind to RET by default
    Opening posts from the *org-blog* buffer is probably the most complex part of
    the system.
    
    org-blog-mode is caching two data structures: the vector of posts pulled from
    the server which is used to display a list of posts in the *org-blog*
    buffer (stored as buffer local variable `posts-cache`) and a hash table which
    maps from a filename to a full file path.
    
    The hash table is also stored on the local file system at the path specified by
    the `org-blog-local-data-file` configuration variable.
    
    When a user posts a buffer to an org blog server, an entry is added to the hash
    table so that the post's path can quickly be looked up when the user
    wants to open the post from the *org-blog* buffer. The path of the post relative to the
    user's home directory is also sent to the server to be stored with the post.
    
    The path that's stored on the server is meant to make it easy to sync posts
    across different machines. Consider the following use case:
    1. I write a blog post in ~/blog/new_post.org and run `org-blog-post-buffer`
    2. On a different computer, I run `org-blog` pointing to the same server
    3. I see the new post in the list and attempt to open it for editing
    4. Here there are two cases:
    A. I'm using some type of file synchronization system (like SyncThing), so a
    file at ~/blog/new_post.org already exists on the other computer. In this case,
    org-blog-mode will find the file using the path from the server.
    B. I don't have the post on the other computer, so I need to pull it down from
    the server. In this case, org-blog-mode will prompt me as to where to store the
    post (defaulting to the path from the server), get the raw post from the server,
    and write it to the file at the specified path.
    
    This is probably a sort of rare use-case, but it will make using org-blog-mode a
    little bit more convenient, at least for me.
    
    This use-case also got me thinking about trying to make it possible to use the
    org-blog server as a git remote. This would be an alternative to using
    org-blog-mode and would be a more generic solution. With this approach,
    org-blog-mode is probably a bit redundant. magit and dired would be enough of an
    interface to manage posts.

--- org-blog.mode.el
+++ org-blog.mode.el
@@ -1,3 +1,4 @@
+;; -*- lexical-binding: t -*-
 (use-package dash)
 (use-package dash-functional :after dash)
 (use-package s)
@@ -133,6 +134,13 @@
            :success success-callback
            :error org-blog--handle-error))
 
+(defun org-blog--get-raw-post (filename success-callback)
+  (request (s-concat org-blog-server-url "/post/" filename "?raw=1")
+           :type "GET"
+           :parser 'json-read
+           :success success-callback
+           :error org-blog--handle-error))
+
 ;;;; Org Blog Mode
 (defun org-blog--hide-cursor ()
   (setq-local cursor-type nil)
@@ -143,6 +151,7 @@
   (org-blog--hide-cursor))
 
 (define-key org-blog-mode-map (kbd "D") 'org-blog-delete-post-at-point)
+(define-key org-blog-mode-map (kbd "RET") 'org-blog-open-post-at-point)
 
 (defun org-blog ()
   "Open an org-blog buffer for managing posts on an org-blog server"
@@ -155,34 +164,66 @@
     (org-blog--hide-cursor)))
 
 (defun org-blog--redraw-buffer ()
- (with-current-buffer (get-buffer-create "*org-blog*")
-   (setq-local line-before-redraw (line-number-at-pos (point)))
-   (-let ((buffer-read-only nil)
-          (line-before-redraw (line-number-at-pos (point))))
-     (save-excursion
-       (setq header-line-format (org-blog--get-header-line))
-       (erase-buffer)
-       (goto-char (point-min))
-       (insert (org-blog--get-label-line))
-       (-let [list-text (->> posts-cache
-                             (-sort org-blog--sort-comparator)
-                             (-map 'org-blog--post-data->display-text)
-                             (s-join ""))]
-         (insert list-text))
-       (goto-char (point-min))
-       (align-regexp (point) (point-max)
-                     "\\(\\s-*\\)\\s-" 1 5 t))
-     (forward-line (1- (min line-before-redraw
+  "Fill the *org-blog* buffer based on the data
+   in the buffer local variable `posts-cache'"
+  (setq-local line-before-redraw (line-number-at-pos (point)))
+  (-let ((buffer-read-only nil)
+         (line-before-redraw (line-number-at-pos (point))))
+    (save-excursion
+      (setq header-line-format (org-blog--get-header-line))
+      (erase-buffer)
+      (insert (org-blog--get-label-line))
+      (-let [list-text (->> posts-cache
+                            (-sort org-blog--sort-comparator)
+                            (-map 'org-blog--post-data->display-text)
+                            (s-join ""))]
+        (insert list-text))
+      (goto-char (point-min))
+      (align-regexp (point) (point-max)
+                    "\\(\\s-*\\)\\s-" 1 5 t))
+    (forward-line (1- (min line-before-redraw
                            (1- (line-number-at-pos (point-max)))))))
-   (org-blog--hide-cursor)))
+  (org-blog--hide-cursor))
+
+(defun org-blog--open-post (filename)
+  "Find and open the post with the given filename.
+   If it can't be found, prompt the user to specify
+   a location to create it and pull the raw post
+   contents from the server."
+  (-let [cached-path (gethash filename (org-blog--get-or-create-file-hash) nil)]
+    (if (and cached-path (file-exists-p cached-path))
+        (switch-to-buffer (find-file cached-path))
+      (-let* ((post (seq-find
+                     (lambda (p)
+                       (string= filename
+                                (alist-get 'filename p)))
+                     posts-cache))
+              (full-path (substitute-in-file-name
+                          (s-concat "$HOME" (alist-get 'path_relative_to_home post)))))
+        (if (file-exists-p full-path)
+            (switch-to-buffer (find-file full-path))
+          (-let [new-path
+                 (read-file-name "Post not found locally. Where should it be created? " full-path full-path)]
+            (org-blog--update-file-hash filename new-path)
+            (org-blog--get-raw-post
+             filename
+             (cl-function (lambda (&key data &allow-other-keys)
+                            (-let [new-buff (find-file new-path)]
+                              (with-current-buffer new-buff
+                                (erase-buffer)
+                                (insert (alist-get 'post data))
+                                (save-buffer)
+                                (goto-char (point-min)))
+                              (switch-to-buffer new-buff)))))))))))
 
 (defun org-blog-refresh ()
   "Request posts from the server and refresh the org-blog buffer"
   (interactive)
   (org-blog--list
    (cl-function (lambda (&key data &allow-other-keys)
-                  (setq-local posts-cache data)
-                  (org-blog--redraw-buffer)))))
+                  (with-current-buffer (get-buffer-create "*org-blog*")
+                    (setq-local posts-cache data)
+                    (org-blog--redraw-buffer))))))
 
 (defun org-blog-post-buffer ()
   "Post the contents of the current buffer
@@ -199,17 +240,28 @@
   (org-blog--post "true"))
 
 (defun org-blog-delete-buffer-post ()
+  "Delete the post currently being visited"
   (interactive)
   (org-blog--delete (org-blog--get-current-filename)
                     (cl-function (lambda (&key data &allow-other-keys)
-                                   (message "Post removed")))))
+                                   (message "Post removed")
+                                   (org-blog-refresh)))))
+
+(defun org-blog--point-is-on-post ()
+  (if (> (line-number-at-pos (point)) 1)
+      t
+    (message "No post is highlighted")))
 
 (defun org-blog-delete-post-at-point ()
   (interactive)
-  (if (> (line-number-at-pos (point)) 1)
-      (org-blog--delete (org-blog--get-filename-at-point)
-                        (cl-function (lambda (&key data &allow-other-keys)
-                                       (org-blog-refresh))))
-    (message "No post is highlighted")))
+  (when (org-blog--point-is-on-post)
+    (org-blog--delete (org-blog--get-filename-at-point)
+                      (cl-function (lambda (&key data &allow-other-keys)
+                                     (org-blog-refresh))))))
+
+(defun org-blog-open-post-at-point ()
+  (interactive)
+  (when (org-blog--point-is-on-post)
+    (org-blog--open-post (org-blog--get-filename-at-point))))
 
 (provide 'org-blog)
